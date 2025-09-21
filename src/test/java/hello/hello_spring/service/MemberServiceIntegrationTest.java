a1d3678e99b367428e916240566792f82d025110
developerjjoa <developerjjoa@gmail.com>
2025-09-21 22:25:32 +0900

    스프링 통합 테스트 - MemberServiceIntegrationTest, 스프링 컨테이너와 DB까지 연결한 통합 테스트로 @SpringBootTest를 달면 스프링 부트 애플리케이션 전체를 실행해서 스프링 컨테이너를 띄우고 거기서 빈을 주입 받아 통합 테스트를 할 수 있다. 이 애노테이션이 없으면 스프링 컨테이너가 뜨지 않기 때문에  @Autowired가 동작하지 않는다. 단순 자바 단위 테스트가 아니라, 스프링 빈 주입까지 포함된 통합 테스트가 가능하다. 테스 코드에 붙은 @Transactional은 테스트가 끝난 뒤에는 자동으로 rollback을 해준다. 테스트는 반복 실행해야 하고, 데이터가 누적되면 결과가 달라질 수 있기 때문에 매번 테스트가 깨끗한 상태의 DB에서 실행됨.

---
+++ src/test/java/hello/hello_spring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,104 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        //then
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+/*
+        try {
+            memberService.join(member2);
+            fail();
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        }
+*/
+
+        //then
+    }
+
+    @Test
+    void findMembers() {
+        //given
+        Member member1 = new Member();
+        member1.setName("member1");
+        memberService.join(member1);
+
+        Member member2 = new Member();
+        member2.setName("member2");
+        memberService.join(member2);
+
+        //when
+        List<Member> findMembers = memberService.findMembers();
+
+        //then
+        assertThat(findMembers.size()).isEqualTo(2);
+        assertThat(findMembers).contains(member1, member2);
+    }
+
+    @Test
+    void findOne() {
+        //given
+        Member member1 = new Member();
+        member1.setName("member1");
+        Long saveId1 = memberService.join(member1);
+
+        Member member2 = new Member();
+        member2.setName("member2");
+        Long saveId2 = memberService.join(member2);
+
+        //when
+        Optional<Member> foundMember = memberService.findOne(saveId1);
+
+        //then
+        assertThat(foundMember).isPresent();
+        assertThat(foundMember.get()).isEqualTo(member1);
+    }
+}
