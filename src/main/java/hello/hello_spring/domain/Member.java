f40bd70151067dc230de3098336cd03643e205de
developerjjoa <developerjjoa@gmail.com>
2025-09-21 22:36:55 +0900

    스프링 통합 테스트 - 테스트 코드의 findMembers(), findOne()은 DB에서 꺼낸 멤버 객체와 테스트 코드에서 생성한 멤버 객체는 서로 다른 인스턴스이기 때문에 테스트 실패를 하는데, 실패를 하는 이유는 엔티티 비교 방식 때문이다. 참조값이 달라서 실패가 하는 것이기 때문에 equals()/hashCode()를 오버라이딩 하지 않으면 같은 객체로 취급되지 않는다. 그래서 Member 클래스에 id 기준으로 오버라이딩 해주고 나니 테스트가 잘 통과된다.

--- src/main/java/hello/hello_spring/domain/Member.java
+++ src/main/java/hello/hello_spring/domain/Member.java
@@ -1,5 +1,7 @@
 package hello.hello_spring.domain;
 
+import java.util.Objects;
+
 public class Member {
     private Long id;
     private String name;
@@ -18,5 +20,18 @@ public class Member {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return Objects.equals(id, member.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
